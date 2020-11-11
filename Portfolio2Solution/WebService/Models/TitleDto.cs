67385e315676194b5d4a1dee9f7e4ecee5394ab2
MD <danielledadie05@gmail.com>
2020-11-11 19:10:48 +0100

    title dto

--- Portfolio2Solution/WebService/Models/TitleDto.cs
+++ Portfolio2Solution/WebService/Models/TitleDto.cs
@@ -3,7 +3,6 @@ namespace WebService.Models
 {
     public class TitleDto
     {
-        public string Url { get; set; }
         public string Type { get; set; }
         public string PrimaryTitle { get; set; }
         public string StartYear { get; set; }
