bc37bbf4a9ec75a02c6ec6158113611019a01f04
Marko Zivko <markozivko@Markos-MacBook-Pro.local>
2020-11-16 20:44:32 +0100

    Add test for data layer

---
+++ Portfolio2Solution/UnitTests/TitleControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using WebService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using DataServiceLibrary;
+using Moq;
+using DataServiceLibrary.Models;
+using AutoMapper;
+using WebService.Models;
+
+namespace UnitTests
+{
+    public class TitleControllerTests
+    {
+
+        [Fact]
+        public void GetTitleWithValidIdShouldReturnOk()
+        {
+            var dataServiceMock = new Mock<IDataService>();
+            dataServiceMock.Setup(x => x.GetTitle("tt0098769")).Returns(new TitleBasics());
+
+            var mapperMock = new Mock<IMapper>();
+            mapperMock.Setup(x => x.Map<TitleDto>(It.IsAny<TitleBasics>())).Returns(new TitleDto());
+
+            var UrlMock = new Mock<IUrlHelper>();
+
+            var ctrl = new TitleController(dataServiceMock.Object, mapperMock.Object);
+            ctrl.Url = UrlMock.Object;
+
+            var response = ctrl.GetTitle("tt0098769");
+
+            response.Should().BeOfType<UnauthorizedResult>();
+        }
+    }
+}
---
+++ Portfolio2Solution/UnitTests/UnitTest1.cs
@@ -0,0 +1,50 @@
+using System;
+using DataServiceLibrary;
+using DataServiceLibrary.Models;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        private readonly string _connectionString;
+        public UnitTest1()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .Build();
+            _connectionString = config["connectionString"];
+        }
+        [Fact]
+        public void GetUserThatExists()
+        {
+
+            var service = new DataService(_connectionString);
+            var user = service.GetUser(1);
+            Assert.Equal("Annelise", user.FirstName);
+
+        }
+
+        [Fact]
+        public void GetUserThatIsNotStaff()
+        {
+
+            var service = new DataService(_connectionString);
+            var user = service.GetUser(1);
+            Assert.False(user.IsStaff);
+
+        }
+
+        [Fact]
+        public void GetAllTitlesByGenre()
+        {
+
+            var service = new DataService(_connectionString);
+            var title = service.NumberOfTitleByGenre(2);
+            Assert.Equal(707, title);
+
+        }
+    }
+}
